a9b44e2608063fa7c271af2eaeff588cd436c3cd
shinx-o <ayushchamp20@gmail.com>
2022-06-08 16:11:35 +0530

    created the featured screen for the Home

--- client/src/mycomponents/featured/Featured.jsx
+++ client/src/mycomponents/featured/Featured.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import './Featured.scss'
-import pfp from '../../media-files/images/anime1.jpg'
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import featured from '../../media-files/images/anime1.jpg'
+import title from '../../media-files/images/title.png'
 
 export default function Featured() {
     return (
         <div className='featured'>
-            <img src={pfp} alt="" />
+            <img src={featured} alt="" />
+            <div className="info">
+                <img src={title} alt="title" />
+                <span className='desc'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempore, enim! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione alias rerum quasi a. Eligendi officia tenetur sit, repellendus error id modi incidunt impedit deleniti, consectetur quisquam nemo quo explicabo consequatur.</span>
+                <div className="buttons">
+                    <button className="play">
+                        <PlayArrowIcon />
+                        <span>Play</span>
+                    </button>
+                    <button className="more">
+                        <InfoOutlinedIcon />
+                        <span>More Info</span>
+                    </button>
+                </div>
+            </div>
         </div>
     )
 }
